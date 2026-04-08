e622915233c9e0b86d3cb4c2d1627b0f77dcdcc7
Akash Sharma <akashshaxma@gmail.com>
2026-04-08 14:30:53 +0530

    Added full audio system (bg music,boost ,pickup,delivery,crashdriving sound)

--- Build/Web builds.loader.js
+++ Build/Web builds.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 63013859,
+    wasmFileSize: 63030233,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
